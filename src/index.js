968b9fc096fb8451627d5e055acf4a6d436c8142
Mr-Hoover <43415739+Mr-Hoover@users.noreply.github.com>
2019-04-17 14:38:00 -0600

    css fix
    scss to css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.scss";
+import "./styles.css";
 
 const sound1 = new Audio(
   "http://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
